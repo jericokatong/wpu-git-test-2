b240857ef95c2ef00f810d7ee4eac0d5d4b8f00f
jericokatong <jericokatong@gmail.com>
2022-07-06 15:53:01 +0900

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae neque suscipit fuga tempore eaque minus, maxime facere cum quo repellendus quos accusantium quam, animi unde quasi ex cupiditate libero reprehenderit.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
